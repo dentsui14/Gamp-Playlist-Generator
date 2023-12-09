ce993fc4bef8209c52e58c5e810279c8ee61651e
dentsui14 <58315782+dentsui14@users.noreply.github.com>
2023-12-08 20:00:39 -0500

    Add files via upload

---
+++ client/script.js
@@ -0,0 +1,86 @@
+const form = document.forms.myForm;
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+
+    console.log("Form data")
+    console.log(formData)
+
+    const playlistSize = formData.get('duration');
+    const artistName = formData.get('artist');
+    const songTitle = formData.get('song');
+    const playlistYear = parseInt(formData.get('period').slice(0, -1), 10);
+
+    const data = {
+        playlistSize: playlistSize ? playlistSize : null,
+        artistName: artistName ? artistName : null,
+        songTitle: songTitle ? songTitle : null,
+        playlistYear: playlistYear ? playlistYear : 0,
+    };
+
+    console.log("Data")
+    console.log(data)
+    
+    const response = await fetch('http://localhost:5000/api/generate_music', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    const responseData = await response.json();
+
+    console.log("Response data");
+    console.log(responseData);
+
+    if (response.status === 422) {
+        console.error("Validation errors:", responseData.detail);
+    }
+
+    // Save data to local storage
+    localStorage.setItem('playlistData', JSON.stringify(responseData));
+
+    // Redirect to the output page
+    window.location.href = 'output.html';
+});
+
+async function fetchArtists() {
+    const response = await fetch("http://localhost:5000/api/artists");
+    const data = await response.json();
+    return data;
+  }
+  
+async function fetchSongs() {
+    const response = await fetch("http://localhost:5000/api/songs");
+    const data = await response.json();
+    return data;
+}
+  
+async function populateDropdowns() {
+    const artists = await fetchArtists();
+    const artistDropdown = document.getElementById("artists");
+    
+
+    artists.forEach((artist) => {
+        const option = document.createElement("option");
+        option.value = artist;
+        option.text = artist;
+        artistDropdown.appendChild(option);
+    });
+    
+    const songs = await fetchSongs();
+    const songDropdown = document.getElementById("songs");
+  
+    songs.forEach((song) => {
+      const option = document.createElement("option");
+      option.value = song;
+      option.text = song;
+      songDropdown.appendChild(option);
+    });
+  }
+  
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM loaded")
+    populateDropdowns();
+});
